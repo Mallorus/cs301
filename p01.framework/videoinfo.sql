7cfbb25ad4fee6021271f0313e4202575ba383bf
ufidon <ufidon@gmail.com>
2016-11-02 11:32:39 -0500

    database uploaded

---
+++ p01.framework/videoinfo.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 02, 2016 at 04:29 PM
+-- Server version: 5.6.30
+-- PHP Version: 5.5.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `videoinfo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE IF NOT EXISTS `videos` (
+  `ID` int(11) NOT NULL,
+  `NAME` varchar(30) NOT NULL,
+  `TYPE` varchar(30) NOT NULL,
+  `SIZE` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `videos`
+--
+
+INSERT INTO `videos` (`ID`, `NAME`, `TYPE`, `SIZE`) VALUES
+(1, 'fire', 'mp4', 1234),
+(2, '1419747936_PlantUML-NB-1.5.nbm', '', 0),
+(3, '1419747936_PlantUML-NB-1.5.nbm', '', 0),
+(4, '1419747936_PlantUML-NB-1.5.nbm', '', 0),
+(5, 'mydll.c', 'text/plain', 68);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `ID` (`ID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
